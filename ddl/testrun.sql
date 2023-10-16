c9cb17a2295c3b2123d0cd51ae9456c5e4a1aed4
David Rolfe <drolfe@voltdb.com>
2023-10-16 11:16:03 +0100

    Fixed messages in testrun.sql

--- ddl/testrun.sql
+++ ddl/testrun.sql
@@ -6,29 +6,28 @@
 -- Or you could start a sqlcmd session and cut and paste the commands..
 --
 
-echo user does not exist
+echo Test 1: user does not exist
 exec SpendMoney 1000 1 1 10 1;
 
-echo zip code does not exist
+echo Test 2: zip code does not exist
 exec SpendMoney 4 1 1 10 1;
 
-echo transacton is too expensive
+echo Test 3: transaction is too expensive
 exec SpendMoney 3 1 1 10 10000;
 
-echo A valid purchase
+echo Test 4: A valid purchase
 exec SpendMoney 1 1 1 10 1;
 
-echo Query customer 1
+echo Test 5: Query customer 1
 exec QueryCustomer 1 1 1;
 
-echo Another valid purchase
+echo Test 6: Another valid purchase
 exec SpendMoney 2 50 1 10 1;
 
-echo Query customer 2
+echo Test 7: Query customer 2
 exec QueryCustomer 2 2 2 ;
 
-
-echo show sales by zip
+echo Test 8: show sales by zip
 SELECT * FROM purchase_history_by_zip;
 
 
